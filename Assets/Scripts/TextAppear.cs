24711f90e100fa35337f1bd031236252e88a6429
Julija Zitkova <main.zhitkova@gmail.com>
2023-04-24 03:16:49 +0300

    Sāku taisīt pašu spēle

---
+++ Assets/Scripts/TextAppear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppear : MonoBehaviour {
+
+	void Update () {
+		
+	}
+	public string text;
+	public GameObject inputField;
+	public GameObject textAppear;
+	public void saveText(){
+		text = inputField.GetComponent<Text> ().text;
+		textAppear.GetComponent<Text>().text = "Hey" +text.ToUpper()+"!";
+	}
+
+}
