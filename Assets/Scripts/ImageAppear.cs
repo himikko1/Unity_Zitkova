103745bdfa2fd24affae6f576d904301dab204f4
Yulia <main.zhitkova@gmail.com>
2023-04-24 14:11:39 +0300

    Izveidoju Toggle, kur var ieslegt vai izslegt Charmander

--- Assets/Scripts/ImageAppear.cs
+++ Assets/Scripts/ImageAppear.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ImageAppear : MonoBehaviour {
 	public GameObject pikachu;
+	public GameObject charmander;
 
 	public void pikachuAppear(bool value){
 		pikachu.SetActive (value);
+	}
+	public void charmanderAppear(bool value){
+		charmander.SetActive (value);
 	}
 
 
