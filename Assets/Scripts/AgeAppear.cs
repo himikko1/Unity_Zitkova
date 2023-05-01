224312cb52ca7a9517b6452392728e0f4d7b2362
Julija Zitkova <main.zhitkova@gmail.com>
2023-05-02 00:39:22 +0300

    Uzlaboju

--- Assets/Scripts/AgeAppear.cs
+++ Assets/Scripts/AgeAppear.cs
@@ -1,23 +1,18 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AgeAppear : MonoBehaviour {
 
-	public GameObject inputField2; // Здесь мы объявляем переменную inputField2 типа InputField, чтобы получить доступ к полю ввода
-	public GameObject ageAppear; // Здесь мы объявляем переменную ageAppear типа Text, чтобы вывести на экран возраст
+	void Update () {
 
-	public void saveTextOfAge(){
-		int age;
-		if (int.TryParse(inputField2.text, out age)) { // Здесь мы проверяем, что пользователь ввел число
-			ageAppear.text = age + " years old!"; // Здесь мы выводим возраст на экран
-		} else {
-			Debug.Log("Ошибка ввода. Введите число."); // Здесь мы выводим сообщение об ошибке в консоль Unity
-		}
+	}
+	public GameObject text;
+	public GameObject inputAge;
+	public void showTextAge(){
+		text.GetComponent<Text> ().text=
+			"I am "+ inputAge.GetComponent<Text>().text+" years old!";
 	}
 
-	private void Start() {
-		inputField2.contentType = InputField.ContentType.IntegerNumber; // Здесь мы устанавливаем тип контента поля ввода на "целое число"
-	}
-}*/
+}
--- Assets/Scripts/ImageAppear.cs
+++ Assets/Scripts/ImageAppear.cs
@@ -1,17 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageAppear : MonoBehaviour {
 	public GameObject pikachu;
+	public GameObject variableImage;
 	public GameObject charmander;
+	public Sprite[] imageArray;
+	public GameObject partyHat;
+	public void partyHatAppear(bool value){
+		partyHat.SetActive (value);
+	}
+	public void showPokemon(int which){
+		if (which == 0) {
+			pikachu.SetActive (true);
+			charmander.SetActive (false);
+		} else {
+			pikachu.SetActive (false);
+			charmander.SetActive (true);
+		}
+
+	}
+	void Start(){
+		charmander.SetActive (false);
+	}
+	/*public void dropDown(int num){
+		if (num == 0) {
+			//Debug.Log("Pikachu");
+			variableImage.GetComponent<Image>().sprite = imageArray [0];
+		}
+		if (num == 1) {
+			//Debug.Log("Charmander");
+			variableImage.GetComponent<Image>().sprite = imageArray [1];
+		}
+	}
 
 	public void pikachuAppear(bool value){
 		pikachu.SetActive (value);
 	}
 	public void charmanderAppear(bool value){
 		charmander.SetActive (value);
-	}
-
+	}*/
 
 }
